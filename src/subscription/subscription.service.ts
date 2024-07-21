90c594aaea323b54e660e5a971d7d3ec45eb87d9
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2024-07-21 22:58:17 +0100

    Feat: debug subscription

--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -135,7 +135,7 @@ export class SubscriptionService {
   }
 
   private verifySignature(req: Request): boolean {
-    this.logger.warn('Verifying signature', req.body);
+    this.logger.warn('Verifying signature', this.secretKey, req.body);
     const hash = crypto
       .createHmac('sha512', this.secretKey)
       .update(JSON.stringify(req.body))
